0d30d6ada0c5c146f02281021846726cf24e8d80
NowGoDigital <prodigital1212@gmail.com>
2026-09-11 16:45:39 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,2 +1,64 @@
-// File ini diisi otomatis oleh CMS
-window.SITE_DATA = null;
+window.SITE_DATA = {
+  "KONFIGURASI": {
+    "namaTravel": "Al-Barokah Travel",
+    "nomorWA": "6281234567890",
+    "pesanWADefault": "Assalamu'alaikum.",
+    "alamat": "-",
+    "telepon": "-",
+    "email": "-",
+    "jamOperasional": "-",
+    "izinKemenag": "-",
+    "sosmed": {
+      "facebook": "#",
+      "instagram": "#",
+      "youtube": "#",
+      "tiktok": "#"
+    }
+  },
+  "SLIDES": [],
+  "PAKET": [
+    {
+      "id": 1,
+      "nama": "UMROH PAKET 12 hari",
+      "jenis": "umroh-reguler",
+      "labelJenis": "Umroh Reguler",
+      "foto": "",
+      "harga": 30000000,
+      "hargaPromo": 25000000,
+      "durasi": 12,
+      "tanggalBerangkat": "2026-12-11",
+      "maskapai": "-",
+      "hotelMakkah": {
+        "nama": "Tower Zam Zam",
+        "bintang": 5,
+        "jarak": ""
+      },
+      "hotelMadinah": {
+        "nama": "Al Madinah",
+        "bintang": 4,
+        "jarak": ""
+      },
+      "kuota": 40,
+      "sisaKuota": 2,
+      "status": "open",
+      "fasilitas": [
+        "Koper",
+        "Seragam",
+        "Pakaian Ihram"
+      ],
+      "perlengkapan": [],
+      "itinerary": [
+        {
+          "hari": 0,
+          "judul": "Madinah",
+          "ket": "Langsung Hotel"
+        }
+      ]
+    }
+  ],
+  "ALUR": [],
+  "GALERI": [],
+  "TESTIMONI": [],
+  "ARTIKEL": [],
+  "FAQ": []
+};
